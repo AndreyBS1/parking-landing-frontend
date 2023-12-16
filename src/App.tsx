56907c89aa550466a6a94286e6b79c959cdff909
AndreyBS1 <andrey.babenkov.00@gmail.com>
2023-12-16 17:28:45 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Parking Landing</h1>
+    </div>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      white: '#ffffff',
+      black: '#000000',
+      'black-transparent': {
+        70: '#000000B2',
+      },
+      primary: '#F9B004',
+      'primary-accent': '#9A8452',
+      secondary: '#D9D9D9',
+      'secondary-accent': '#E4E4E4'
+    },
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
